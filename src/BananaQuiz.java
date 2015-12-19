3187eb9da285162539a141f369c14b8868c5f8af
BigRockerEnding <bigrockerending@live.com>
2015-12-19 12:02:10 -0800

    Banana!!!

---
+++ src/BananaQuiz.java
@@ -0,0 +1,31 @@
+// Copyright Wintriss Technical Schools 2013
+import javax.swing.JOptionPane;
+
+public class BananaQuiz
+{
+	
+	public static void main(String[] args)
+	{
+		//1. ask the user if they like bananas
+		for (int i = 0; i < 3; i++) {
+			
+		}
+		String dino=
+		JOptionPane.showInputDialog(null,"Do you like Banana's???");
+	
+		//2. if they say no, 
+			//tell them they are crazy 
+			//and end quiz
+		if  (dino.equals("yes")) {
+			JOptionPane.showMessageDialog(null,"You are Awesome!!!");
+		}
+		if (dino.equals("no")){
+			JOptionPane.showMessageDialog(null, "Your weird");
+		}
+		//3. if they say yes
+		//	ask them what is their favorite hobby
+		//	show a pop up that says "<your hobby> is much better with bananas!"
+	
+	}
+
+}
