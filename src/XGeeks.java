70ef1575e723ce4f4b6b93ae86f3aab23439e5b2
ThisSteveIsReal2 <StarWars@Khias-MacBook-Air.local>
2016-01-09 12:04:59 -0800

    Sup Powers

--- src/XGeeks.java
+++ src/XGeeks.java
@@ -13,25 +13,46 @@ public class XGeeks {
 
 		// 1. Save the superpower for each person in a variable.
 		String Bob = "You can eat 10 pizza's and become a cat";
-		String Noelani = "Adaptobility";
-		String Nancy = "Motovater";
+		String Noelani = "You have the ability to Adaptobility";
+		String Nancy = " You are a Motovater";
+		String Joon = "You have Jedi mind tricks";
+		String Kira = "You can run supa dupa fast";
+		String Khia = "You have the ability to change into a Alicorn";
+		String Courtaney ="You are the best person in the entire world";
+		String Vinh = "You are the best vido gamer in the world";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
 		String Dino = JOptionPane.showInputDialog(null, "What is your name?");
 
-		JOptionPane.showMessageDialog(null, "What is your name?");
+		
 
 		// 3. Show the superpower in a pop-up, depending on the name entered.
 		if (Dino.equals("Bob")) {
-JOptionPane.showMessageDialog(null, Bob);
+			JOptionPane.showMessageDialog(null, Bob);
 		}
 		
 		if (Dino.equals("Nancy")) {
-			
+			JOptionPane.showMessageDialog(null, Nancy);
 		}
 		
 		if (Dino.equals("Noelani")) {
-			
+			JOptionPane.showMessageDialog(null, Noelani);
 		}
 		
-	}
+		if (Dino.equals("Joon")) {
+			JOptionPane.showMessageDialog(null, Joon);
+				}
+		
+		if (Dino.equals("Kira")) {
+			JOptionPane.showMessageDialog(null, Kira);
+			}
+		if (Dino.equals("Khia")) {
+			JOptionPane.showMessageDialog(null, Khia);
+}
+		if (Dino.equals("Courtaney")) {
+			JOptionPane.showMessageDialog(null, Courtaney);
+		}
+		if (Dino.equals("Vinh")) {
+			JOptionPane.showMessageDialog(null, Vinh);
+		}
+}
 }
