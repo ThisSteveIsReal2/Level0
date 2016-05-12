f127f31b871b3ad1572c67b5361592eee23d56bb
ThisSteveIsReal2 <league@Mac-mini-2.local>
2016-05-11 18:56:21 -0700

    Try

---
+++ src/FizzBuzz.java
@@ -0,0 +1,34 @@
+
+public class FizzBuzz {
+public static void main(String[] args) {
+	for (int i=1; i < 1000; i++){
+		System.out.println(i); 
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
+	
+}
---
+++ src/LotteryNumbers.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class LotteryNumbers {
+public static void main(String[] args) {
+	for (int i = 0; i < 5; i++) {
+		
+	
+	int Steve = new Random ().nextInt (100);
+	System.out.println(Steve);
+	JOptionPane.showMessageDialog(null,Steve);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+}
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
